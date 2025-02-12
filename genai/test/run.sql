d4c665c73ba5ba9862aff0133a911ddef7a8e806
khkwon01 <khkwon01@gmail.com>
2025-02-12 17:50:28 +0900

    Update run.sql

--- genai/test/run.sql
+++ genai/test/run.sql
@@ -16,6 +16,19 @@ SELECT @review AS "--- New review ---";
 SELECT sentiment AS "--- Sentiment of the new review ---" FROM reviews WHERE id = @review_id;
 
 
+SELECT '################### 제품에 대한 고객에 review 내용들을 요약 ###################' AS '';
 
-CALL SUMMARIZE_TRANSLATE(1, "긍정", "ko", @positive_english_summary);
-SELECT @positive_english_summary AS "--- 티셔츠에 대한 긍정적인 리뷰의 업데이트된 영어 요약 ---";
+CALL SUMMARIZE_TRANSLATE(1, "긍정", "ko", @positive_korea_summary);
+SELECT @positive_korea_summary AS "--- 티셔츠에 대한 긍정적인 리뷰들에 대한 업데이트된 내용 요약 ---";
+
+CALL SUMMARIZE_TRANSLATE(1, "부정", "ko", @negative_korea_summary);
+SELECT @negative_korea_summary AS "--- 티셔츠에 대한 긍정적인 리뷰들에 대한 업데이트된 내용 요약 ---";
+
+
+SELECT '################### 제품에 대한 고객에 review 내용들을 다른 언어로 번역 ###################' AS '';
+
+CALL SUMMARIZE_TRANSLATE(1, "긍정", "en", @positive_english_summary);
+SELECT @positive_english_summary AS "--- English summary of positive reviews on the T-Shirt ---";
+
+CALL SUMMARIZE_TRANSLATE(1, "부정", "en", @negative_english_summary);
+SELECT @negative_english_summary AS "--- English summary of negative reviews on the T-Shirt ---";
